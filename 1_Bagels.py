3c44d48f7a8fa35238b951068eeaadde480c1f9c
Ilia Vetchinin <ilia@Ilias-MacBook-Pro.local>
2022-01-15 02:08:15 +0300

    Initial commit

---
+++ 1_Bagels.py
@@ -0,0 +1,14 @@
+"""
+This is my version of game Bagels
+
+ © Ilia Vetchinin
+
+ ivetchinin@gmail.com
+
+"""
+
+import random
+
+clui = random.randint(99, 1000)
+
+version = input("Ведите Ваш варианта ч")
