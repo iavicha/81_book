008e1a3bed3a54bfd06c7ab32b9895a3d9b2ba4b
ilia <ivetchinin@gmail.com>
2022-01-16 16:06:45 +0300

    implemented enter function

--- 1_Bagels.py
+++ 1_Bagels.py
@@ -11,4 +11,31 @@ import random
 
 clui = random.randint(99, 1000)
 
-version = input("Ведите Ваш варианта ч")
+number_of_tries = 10
+
+try_ = 0
+
+version_ = ...
+
+
+def enter(error=False):
+    global version_
+    if not error:
+        version_ = input("Ведите Ваш вариант\n")
+    else:
+        version_ = input("Ошибка ввода. Ведите Ваш вариант\n")
+
+    if len(version_) != 3:
+        enter(True)
+
+    try:
+        int(version_)
+
+    except ValueError:
+        enter(True)
+
+    return version_
+
+
+enter()
+
